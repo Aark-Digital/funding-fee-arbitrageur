2b03ad4208062ef2c020902e1db0f64acbd59794
groove <groove@aark.digital>
2023-11-06 23:37:22 +0900

    Replace ccxt to direct http call

--- src/services/okx.service.ts
+++ src/services/okx.service.ts
@@ -1,5 +1,5 @@
-import ccxt, { Exchange } from "ccxt";
-import { sleep } from "../utils/time";
+import axios from "axios";
+import CryptoJS from "crypto-js";
 import { IMarket } from "../interfaces/market-interface";
 import {
   ActionType,
@@ -7,20 +7,23 @@ import {
   ICancelOrderParam,
   ILimitOrderParam,
   IMarketOrderParam,
-  Side,
 } from "../interfaces/order-interface";
 import {
   ceil_dp,
   convertSizeToContractAmount,
   floor_dp,
   numberToPrecision,
-  round_dp,
 } from "../utils/number";
 
 export class OkxSwapService {
-  private client: Exchange;
+  private baseUrl: string = "https://www.okx.com";
   private symbolList: string[];
   private markets: { [symbol: string]: IMarket } = {};
+  private apiInfo: {
+    apiKey: string;
+    secret: string;
+    password: string;
+  };
 
   constructor(
     apiKey: string,
@@ -28,11 +31,11 @@ export class OkxSwapService {
     password: string,
     symbolList: string[]
   ) {
-    this.client = new ccxt.okex({
+    this.apiInfo = {
       apiKey,
       secret,
       password,
-    });
+    };
     this.symbolList = symbolList;
     this.symbolList.forEach((symbol) => {
       this.markets[symbol] = {
@@ -46,23 +49,44 @@ export class OkxSwapService {
   }
 
   async init() {
-    const totalMarketInfo = await this.client.fetchMarkets();
+    const totalMarketInfo = await this._publicGet(
+      "/api/v5/public/instruments",
+      {
+        instType: "SWAP",
+      }
+    );
     this.symbolList.forEach((symbol: string) => {
-      const targetInstId = `${symbol.replace("_", "-")}-SWAP`;
-      const marketInfo = totalMarketInfo.find(
-        (info: any) => info.id === targetInstId
+      const targetInstId = `${this.getFormattedSymbol(symbol)}`;
+      const marketInfo = totalMarketInfo.data.find(
+        (info: any) => info.instId === targetInstId
       )!;
       this.markets[symbol].marketInfo = {
-        contractSize: marketInfo.contractSize!,
-        pricePrecision: numberToPrecision(marketInfo.precision.price!),
-        qtyPrecision: numberToPrecision(marketInfo.precision.amount!),
+        contractSize: Number(marketInfo.ctVal),
+        pricePrecision: numberToPrecision(marketInfo.tickSz),
+        qtyPrecision: numberToPrecision(marketInfo.lotSz),
       };
     });
   }
 
+  private _getSignature(
+    timestamp: Date,
+    requestPath: string,
+    body: string,
+    secretKey: string,
+    method: string
+  ) {
+    const signature = CryptoJS.enc.Base64.stringify(
+      CryptoJS.HmacSHA256(
+        timestamp.toISOString() + method.toUpperCase() + requestPath + body,
+        secretKey
+      )
+    );
+    return signature;
+  }
+
   getFormattedSymbol(symbol: string) {
     const [base, quote] = symbol.split("_");
-    return `${base}/${quote}:${quote}`;
+    return `${base}-${quote}-SWAP`;
   }
 
   getMarketInfo() {
@@ -70,74 +94,103 @@ export class OkxSwapService {
   }
 
   async fetchOrderbooks() {
-    for (const symbol of this.symbolList) {
-      try {
-        const ob = await this.client.fetchOrderBook(
-          this.getFormattedSymbol(symbol)
-        );
+    try {
+      const orderbooks: any[] = await Promise.all(
+        this.symbolList.map((symbol: string) =>
+          this._publicGet("/api/v5/market/books", {
+            instId: this.getFormattedSymbol(symbol),
+            sz: "100",
+          })
+        )
+      );
+
+      this.symbolList.forEach((symbol: string, idx: number) => {
+        const ob = orderbooks[idx].data[0];
         this.markets[symbol].orderbook = {
-          symbol,
-          bids: ob.bids,
-          asks: ob.asks,
-          timestamp: ob.timestamp,
+          symbol: symbol,
+          bids: ob.bids.map((bid: any) => [Number(bid[0]), Number(bid[1])]),
+          asks: ob.bids.map((ask: any) => [Number(ask[0]), Number(ask[1])]),
+          timestamp: Number(ob.ts),
         };
-      } catch (e) {
-        console.log(`[ERROR] Failed to fetch ${symbol} orderbook : ${e}`);
+      });
+    } catch (e) {
+      console.log(`[ERROR] Failed to fetch orderbooks : ${e}`);
+      for (const symbol of this.symbolList) {
         this.markets[symbol].orderbook = undefined;
       }
-      await sleep(100);
     }
   }
 
+  // ONLY CROSS MARGIN IS AVAILABLE
   async fetchPositions() {
-    for (const symbol of this.symbolList) {
-      try {
-        const position = await this.client.fetchPosition(
-          this.getFormattedSymbol(symbol)
+    const timestamp = new Date().getTime();
+    try {
+      const positions = await this._privateGet("/api/v5/account/positions", {
+        instType: "SWAP",
+        instId: this.symbolList.map(this.getFormattedSymbol).join(","),
+      });
+      this.symbolList.forEach((symbol: string, idx: number) => {
+        const contractSize = this.markets[symbol].marketInfo.contractSize;
+        const position = positions.data.find(
+          (pos: any) =>
+            pos.mgnMode === "cross" &&
+            pos.instId === this.getFormattedSymbol(symbol)
         );
-        this.markets[symbol].position = {
-          symbol,
-          price: position.entryPrice ?? 0,
-          size:
-            position.contracts! *
-            position.contractSize! *
-            (position.side === "long" ? 1 : -1),
-          timestamp: new Date().getTime()!,
-        };
-      } catch (e) {
-        console.log(`[ERROR] Failed to fetch ${symbol} position : ${e}`);
-        this.markets[symbol].orderbook = undefined;
+        if (position === undefined) {
+          console.log(
+            `No previous trade in ${this.getFormattedSymbol(symbol)}.`
+          );
+          this.markets[symbol].position = {
+            symbol,
+            timestamp,
+            size: 0,
+          };
+        } else {
+          this.markets[symbol].position = {
+            symbol,
+            size: Number(position.pos) * contractSize,
+            timestamp,
+          };
+        }
+      });
+    } catch (e) {
+      console.log(`[ERROR] Failed to fetch positions : ${e}`);
+      for (const symbol of this.symbolList) {
+        this.markets[symbol].position = undefined;
       }
-      await sleep(100);
     }
   }
 
   async fetchOpenOrders(): Promise<void> {
-    for (const symbol of this.symbolList) {
-      try {
-        const oo = await this.client.fetchOpenOrders(
-          this.getFormattedSymbol(symbol)
+    const timestamp = new Date().getTime();
+    try {
+      const openOrders = await this._privateGet(
+        "/api/v5/trade/orders-pending",
+        {
+          instType: "SWAP",
+        }
+      );
+      this.symbolList.forEach((symbol: string) => {
+        const marketOpenOrders = openOrders.data.filter(
+          (oo: any) => oo.instId === this.getFormattedSymbol(symbol)
         );
         this.markets[symbol].openOrders = {
-          timestamp: new Date().getTime(),
-          openOrders: oo.map((openOrder: any) => ({
+          openOrders: marketOpenOrders.map((oo: any) => ({
+            timestamp,
             symbol,
-            orderId: openOrder.id,
-            price: openOrder.price,
-            size:
-              openOrder.side === "buy" ? openOrder.amount : -openOrder.amount,
-            remaining:
-              openOrder.side === "buy"
-                ? openOrder.remaining
-                : -openOrder.remaining,
-            timestamp: openOrder.timestamp,
+            orderId: oo.ordId,
+            price: Number(oo.px),
+            size: oo.side === "buy" ? Number(oo.sz) : -Number(oo.sz),
+            remaining: Number(oo.fillSz),
           })),
+          timestamp,
         };
-      } catch (e) {
-        console.log(`[ERROR] Failed to fetch ${symbol} open orders : ${e}`);
+      });
+    } catch (e) {
+      console.log(`[ERROR] Failed to fetch open orders : ${e}`);
+      for (const symbol of this.symbolList) {
         this.markets[symbol].openOrders = undefined;
       }
-      await sleep(100);
     }
   }
 
@@ -153,48 +206,113 @@ export class OkxSwapService {
       .filter((param: IActionParam) => param.type === ActionType.CreateLimit)
       .map((param: IActionParam) => param.order) as ILimitOrderParam[];
 
-    await Promise.all(
-      cancelParams.map((param: ICancelOrderParam) =>
-        this.client.cancelOrder(
-          param.orderId,
-          this.getFormattedSymbol(param.symbol)
-        )
-      )
-    );
-    await Promise.all(
-      marketOrderParams.map((param: IMarketOrderParam) =>
-        this.client.createOrder(
-          this.getFormattedSymbol(param.symbol),
-          "market",
-          param.size > 0 ? "buy" : "sell",
-          convertSizeToContractAmount(
-            param.size,
-            this.markets[param.symbol].marketInfo
+    await Promise.all(cancelParams.map(this._cancelOrder));
+    await Promise.all(marketOrderParams.map(this._createMarketOrder));
+    await Promise.all(limitOrderParams.map(this._createLimitOrder));
+  }
+
+  private async _createMarketOrder(param: IMarketOrderParam) {
+    return this._post("/api/v5/trade/order", {
+      instId: this.getFormattedSymbol(param.symbol),
+      tdMode: "cross",
+      side: param.size > 0 ? "buy" : "sell",
+      ordType: "market",
+      sz: `${convertSizeToContractAmount(
+        param.size,
+        this.markets[param.symbol].marketInfo
+      )}`,
+    });
+  }
+
+  private async _createLimitOrder(param: ILimitOrderParam) {
+    return this._post("/api/v5/trade/order", {
+      instId: this.getFormattedSymbol(param.symbol),
+      tdMode: "cross",
+      side: param.size > 0 ? "buy" : "sell",
+      ordType: "limit",
+      sz: convertSizeToContractAmount(
+        param.size,
+        this.markets[param.symbol].marketInfo
+      ).toString(),
+      px: (param.size > 0
+        ? floor_dp(
+            param.price,
+            this.markets[param.symbol].marketInfo.pricePrecision
           )
-        )
-      )
+        : ceil_dp(
+            param.price,
+            this.markets[param.symbol].marketInfo.pricePrecision
+          )
+      ).toString(),
+    });
+  }
+
+  private async _cancelOrder(param: ICancelOrderParam) {
+    return this._post("/api/v5/trade/cancel-order", {
+      instId: this.getFormattedSymbol(param.symbol),
+      ordId: param.orderId,
+    });
+  }
+  private async _publicGet(
+    endPoint: string,
+    params: { [key: string]: string }
+  ) {
+    const response = await axios.get(this.baseUrl + endPoint, {
+      params,
+    });
+    return response.data;
+  }
+
+  private async _privateGet(
+    endPoint: string,
+    params: { [key: string]: string }
+  ): Promise<any> {
+    const timestamp = new Date();
+
+    const headers = {
+      "OK-ACCESS-KEY": this.apiInfo.apiKey,
+      "OK-ACCESS-SIGN": this._getSignature(
+        timestamp,
+        endPoint + "?" + new URLSearchParams(params).toString(),
+        "",
+        this.apiInfo.secret,
+        "GET"
+      ),
+      "OK-ACCESS-TIMESTAMP": timestamp.toISOString(),
+      "OK-ACCESS-PASSPHRASE": this.apiInfo.password,
+      "Content-Type": "application/json",
+    };
+    const response = await axios.get(
+      this.baseUrl + endPoint + "?" + new URLSearchParams(params).toString(),
+      {
+        headers,
+      }
     );
-    await Promise.all(
-      limitOrderParams.map((param: ILimitOrderParam) =>
-        this.client.createOrder(
-          this.getFormattedSymbol(param.symbol),
-          "limit",
-          param.size > 0 ? "buy" : "sell",
-          convertSizeToContractAmount(
-            param.size,
-            this.markets[param.symbol].marketInfo
-          ),
-          param.size > 0
-            ? floor_dp(
-                param.price,
-                this.markets[param.symbol].marketInfo.pricePrecision
-              )
-            : ceil_dp(
-                param.price,
-                this.markets[param.symbol].marketInfo.pricePrecision
-              )
-        )
-      )
-    );
+    return response.data;
+  }
+
+  private async _post(
+    endPoint: string,
+    params: { [key: string]: string }
+  ): Promise<any> {
+    const timestamp = new Date();
+
+    const headers = {
+      "OK-ACCESS-KEY": this.apiInfo.apiKey,
+      "OK-ACCESS-SIGN": this._getSignature(
+        timestamp,
+        endPoint,
+        JSON.stringify(params),
+        this.apiInfo.secret,
+        "POST"
+      ),
+      "OK-ACCESS-TIMESTAMP": timestamp.toISOString(),
+      "OK-ACCESS-PASSPHRASE": this.apiInfo.password,
+      "Content-Type": "application/json",
+    };
+    const response = await axios.post(this.baseUrl + endPoint, params, {
+      headers,
+    });
+    return response.data;
   }
 }
