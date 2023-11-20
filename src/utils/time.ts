03c530c375fc8bd14dd5eafe787f8167af5e8fa9
groove <groove@aark.digital>
2023-11-20 16:15:51 +0900

    Implement logic v1

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,1 +1,5 @@
+export const ONE_DAY_IN_MS = 1000 * 60 * 60 * 24;
+
+export const EIGHT_HOUR_IN_MS = 1000 * 60 * 60 * 8;
+
 export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
