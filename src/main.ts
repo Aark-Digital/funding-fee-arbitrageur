9db551ad43f88702c17d7cd8ba131824b7fd8c2b
groove <groove@aark.digital>
2024-07-11 03:26:20 +0900

    Call if strategy error occurred for many times

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { MonitorService } from "./services/monitor.service";
 import { sleep } from "./utils/time";
 
 require("dotenv").config();
@@ -18,18 +19,39 @@ async function main() {
   const strategyPeriodMs = Number(process.env.STRATEGY_PERIOD_MS);
   if (Number.isNaN(strategyPeriodMs)) {
     throw new Error("Undefined Strategy periods");
+  } else if (
+    process.env.SLACK_URL === undefined ||
+    process.env.TWILIO_PARAM === undefined ||
+    process.env.MANAGER_PARAM === undefined
+  ) {
+    throw Error("Undefined Params for Monitoring");
   }
+  const monitorService = MonitorService.getInstance();
   const { Strategy } = require(`./strategies/${process.env.STRATEGY_NAME}`);
 
   const strategy = new Strategy();
+
   await strategy.init();
+
+  let errCnt = 0;
 
   while (true) {
     try {
       console.log(`~~~~~~~ ${new Date().toISOString()} ~~~~~~~`);
       await strategy.run();
+      errCnt = 0;
     } catch (e) {
       console.log(e);
+      errCnt += 1;
+      if (errCnt > 10) {
+        monitorService.slackMessage(
+          "CONSECUTIVE LOGIC ERROR",
+          `Error occured for ${errCnt} times : ${e}`,
+          60_000,
+          true,
+          true
+        );
+      }
     }
     await sleep(strategyPeriodMs);
   }
